80eeac119ac3749ba0e3ea75dbbb15b43668bce4
Richard Blythman <richardblythman@gmail.com>
2025-01-06 13:05:55 +0000

    use dict for run arg

--- generate_image_agent/run.py
+++ generate_image_agent/run.py
@@ -2,7 +2,8 @@
 import asyncio
 import logging
 from dotenv import load_dotenv
-from naptha_sdk.schemas import AgentDeployment, AgentRunInput
+from typing import Dict
+from naptha_sdk.schemas import AgentDeployment, AgentRunInput, ToolRunInput
 from naptha_sdk.modules.tool import Tool
 from generate_image_agent.schemas import InputSchema, SystemPromptSchema
 
@@ -12,45 +13,53 @@ logger = logging.getLogger(__name__)
 
 class GenerateImageAgent:
 
-    def __init__(self, agent_deployment: AgentDeployment):
-        self.agent_deployment = agent_deployment
-        self.tool = Tool(tool_deployment=self.agent_deployment.tool_deployments[0])
-        self.system_prompt = SystemPromptSchema(role=agent_deployment.config.system_prompt["role"])
+    def __init__(self, deployment: AgentDeployment):
+        self.deployment = deployment
+        self.tool = Tool(tool_deployment=self.deployment.tool_deployments[0])
+        self.system_prompt = SystemPromptSchema(role=self.deployment.config.system_prompt["role"])
 
     async def call_tool(self, module_run: AgentRunInput):
+        tool_run_input = ToolRunInput(
+            consumer_id=module_run.consumer_id,
+            inputs=module_run.inputs,
+            deployment=self.deployment.tool_deployments[0],
+        )
 
-        tool_response = await self.tool.call_tool_func(module_run)
+        tool_response = await self.tool.call_tool_func(tool_run_input)
 
         return tool_response
 
-async def run(module_run: AgentRunInput, *args, **kwargs):
-    logger.info(f"Running with inputs {module_run.inputs.tool_input_data}")
-
+async def run(module_run: Dict, *args, **kwargs):
+    module_run = AgentRunInput(**module_run)
+    module_run.inputs = InputSchema(**module_run.inputs)
     generate_image_agent = GenerateImageAgent(module_run.deployment)
 
-    return await generate_image_agent.call_tool(module_run)
+    tool_response = await generate_image_agent.call_tool(module_run)
+
+    return tool_response
 
 
 if __name__ == "__main__":
+    import os
     from naptha_sdk.client.naptha import Naptha
-    from naptha_sdk.configs import load_agent_deployments, load_tool_deployments
+    from naptha_sdk.configs import setup_module_deployment
 
     naptha = Naptha()
 
     # Configs
-    agent_deployments = load_agent_deployments("generate_image_agent/configs/agent_deployments.json", load_persona_data=False, load_persona_schema=False)
+    deployment = asyncio.run(setup_module_deployment("agent", "generate_image_agent/configs/deployment.json", node_url = os.getenv("NODE_URL"), user_id=naptha.user.id))
 
-    input_params = InputSchema(
-        tool_name="generate_image_tool",
-        tool_input_data="generate an image of a cat",
-    )
+    input_params = {
+        "tool_name": "generate_image_tool",
+        "tool_input_data": "generate an image of a cat",
+    }
 
-    module_run = AgentRunInput(
-        inputs=input_params,
-        agent_deployment=agent_deployments[0],
-        consumer_id=naptha.user.id,
-    )
+    module_run = {
+        "inputs": input_params,
+        "deployment": deployment,
+        "consumer_id": naptha.user.id,
+    }
 
     response = asyncio.run(run(module_run))
 
-
+    print("Response: ", response)
